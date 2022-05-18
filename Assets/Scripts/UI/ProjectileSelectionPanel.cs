45f9df1f4cc94bb029880b9f5f2f1ade897ed268
jellyfishhunter <jellyfishhunter@gmail.com>
2022-05-18 23:34:41 +0200

    Add UI textures

--- Assets/Scripts/UI/ProjectileSelectionPanel.cs
+++ Assets/Scripts/UI/ProjectileSelectionPanel.cs
@@ -1,4 +1,7 @@
+using DG.Tweening;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ProjectileSelectionPanel : MonoBehaviour
 {
@@ -6,16 +9,21 @@ public class ProjectileSelectionPanel : MonoBehaviour
     private GameObject[] _projectilePrefabs;
     [SerializeField]
     private CannonManager _cannon;
+    [SerializeField]
+    private Transform _selector;
+    private Transform[] _buttons;
 
     private void Start()
     {
         _cannon.stateChanged.AddListener(OnCamStateChanged);
+        _buttons = GetComponentsInChildren<Button>().Select(b => b.transform).ToArray();
     }
 
     public void SelectProjectile(int index)
     {
         _cannon.CannonBallPrefab = _projectilePrefabs[index];
         _cannon.State = CannonManager.CannonState.CanFire;
+        _selector.DOLocalMove(_buttons[index].localPosition, .4f);
     }
 
     private void OnCamStateChanged()
